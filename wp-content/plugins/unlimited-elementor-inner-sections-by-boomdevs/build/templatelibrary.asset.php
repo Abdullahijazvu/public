c2e341151c9a7fe48d34a74a2eec7f0d6cd2ae95
Abdullah Ijaz <52850964+Abdullahijazvu@users.noreply.github.com>
2026-06-04 16:43:20 +0500

    new

--- wp-content/plugins/unlimited-elementor-inner-sections-by-boomdevs/build/templatelibrary.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '247b00a3b50b2b161006');
--- wp-content/plugins/unlimited-elementor-inner-sections-by-boomdevs/includes/SiteBuilder/content/content-footer.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly 
-?>
-<footer id="pea-site-builder-footer" class="pea-site-builder-footer">
-	<div class="pea-site-builder-footer-inner">
-		<?php the_content(); ?>
-	</div>
-</footer>
